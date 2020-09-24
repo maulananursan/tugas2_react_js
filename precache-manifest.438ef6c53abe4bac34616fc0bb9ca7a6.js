5611f818831eedfece177d5a435e9b3ecae9cb43
Maulana Nursan <71365575+maulananursan@users.noreply.github.com>
2020-09-25 00:48:47 +0700

    Add files via upload

---
+++ precache-manifest.438ef6c53abe4bac34616fc0bb9ca7a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b00c05a9ff229aa21c62d2eb24d78a41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b27c4a2b595bbe625b9c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "492a82f3aadea1ffa24f",
+    "url": "/static/js/2.dfe18ab1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dfe18ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b27c4a2b595bbe625b9c",
+    "url": "/static/js/main.92c307e3.chunk.js"
+  },
+  {
+    "revision": "3a26d4f1d427b412a021",
+    "url": "/static/js/runtime-main.d95a90f8.js"
+  }
+]);
